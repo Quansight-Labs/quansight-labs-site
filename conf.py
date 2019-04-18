65a6ea90c5ac26e1988dbb353cd6fa5b39c83121
Ralf Gommers <ralf.gommers@gmail.com>
2019-04-18 23:02:16 +0200

    Update deployment config

--- conf.py
+++ conf.py
@@ -557,7 +557,7 @@ REDIRECTIONS = []
 # https://getnikola.com/handbook.html#deploying-to-github
 # You will need to configure the deployment branch on GitHub.
 GITHUB_SOURCE_BRANCH = 'src'
-GITHUB_DEPLOY_BRANCH = 'master'
+GITHUB_DEPLOY_BRANCH = 'gh-pages'
 
 # The name of the remote where you wish to push to, using github_deploy.
 GITHUB_REMOTE_NAME = 'origin'
